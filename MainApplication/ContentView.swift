354e1429ec9752fb24840b63570b21194c572d33
Jogendra <jogendrafx@gmail.com>
2020-09-18 15:35:50 +0530

    Initial Commit

---
+++ MainApplication/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MainApplication
+//
+//  Created by JOGENDRA on 18/09/20.
+//  Copyright © 2020 Jogendra. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
